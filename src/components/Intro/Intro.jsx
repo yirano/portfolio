ced79a8db9f19eb3e283cb59ceb366e7677b4641
yirano <yirano@protonmail.ch>
2019-11-02 20:04:25 -0400

    Video header

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Intro.css';
 import '../../fonts/fonts.css';
+import Video from '../../images/videos/sky.mp4';
 
-export default function Intro() {
-	return (
-		<section className="heading" id="intro">
-			<div className="headingTitle">
-				<p>My name is Tiffany</p>
-				<h1>Hello!</h1>
-				<p className="alignRight">
-					&amp; I'm a <span className="highlight">Frontend Developer</span>
-				</p>
-			</div>
-		</section>
-	);
+export class Intro extends Component {
+	constructor() {
+		super();
+	}
+	render() {
+		return (
+			<section className="heading" id="intro">
+				<video id="backgroundVideo" autoPlay loop>
+					<source src={Video} type="video/mp4" className="backgroundVideo" />
+				</video>
+				<div className="headingTitle">
+					<div className="headingTitleBg" />
+					<div className="headingTitleWrap">
+						<p className="alignLeft">My name is</p>
+						<h1>
+							<span className="highlight">y</span>ir<span className="highlight">a</span>no
+						</h1>
+						<p className="alignRight">&amp; I'm a Frontend Developer</p>
+					</div>
+				</div>
+			</section>
+		);
+	}
 }
+
+export default Intro;
