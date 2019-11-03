c9503d29de52177beb914d5ed1e0299962ec9f7d
yirano <yirano@protonmail.ch>
2019-11-03 13:30:45 -0500

    Add icons to skills section

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import './Intro.css';
-import '../../fonts/fonts.css';
-import HeaderBg from '../../images/headerImg.jpeg';
+import React from "react";
+import "./Intro.css";
+import "../../fonts/fonts.css";
 
 export default function Intro() {
-	return (
-		<section className="heading" id="intro">
-			<div className="headingTitle">
-				<p className="alignLeft uppercase caption">My name is Tiffany</p>
-				<h1>Hello!</h1>
-				<p className="alignRight uppercase caption">
-					&amp; I'm a <span className="highlight">Frontend Developer</span>
-				</p>
-			</div>
-		</section>
-	);
+  return (
+    <section className="heading" id="intro">
+      <div className="headingTitle">
+        <p className="alignLeft uppercase caption">My name is Tiffany</p>
+        <h1>Hello!</h1>
+        <p className="alignRight uppercase caption">
+          &amp; I'm a <span className="highlight">Frontend Developer</span>
+        </p>
+      </div>
+    </section>
+  );
 }
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import "./Skills.css";
+import HTMLCSS from "../../images/icons/htmlcss.png";
+import ReactIcon from "../../images/icons/react.png";
+import GitIcon from "../../images/icons/git.png";
+import Photoshop from "../../images/icons/photoshop.png";
+import Javascript from "../../images/icons/javascript.png";
+import Bootstrap from "../../images/icons/bootstrap.png";
+import Sass from "../../images/icons/sass.png";
+import UX from "../../images/icons/ux.png";
+
+export default function Skills() {
+  return (
+    <section className="skills" id="skills">
+      <div className="skillsText">
+        <div className="skillCards">
+          <div className="skillCard htmlcss">
+            <div className="cardTitle">
+              <img src={HTMLCSS} alt="HTML/CSS icon" />
+              <h4>HTML3/CSS3</h4>
+            </div>
+          </div>
+          <div className="skillCard js">
+            <div className="cardTitle">
+              <img src={Javascript} alt="Javascript icon" />
+              <h4>Javascript</h4>
+            </div>
+          </div>
+          <div className="skillCard bootstrap">
+            <div className="cardTitle">
+              <img src={Bootstrap} alt="Javascript icon" />
+              <h4>Bootstrap</h4>
+            </div>
+          </div>
+          <div className="skillCard react">
+            <div className="cardTitle">
+              <img src={ReactIcon} alt="React icon" />
+              <h4>ReactJS</h4>
+            </div>
+          </div>
+          <div className="skillCard photoshop">
+            <div className="cardTitle">
+              <img src={Photoshop} alt="Photoshop icon" />
+              <h4>Photoshop</h4>
+            </div>
+          </div>
+          <div className="skillCard git">
+            <div className="cardTitle">
+              <img src={GitIcon} alt="Git icon" />
+              <h4>Git</h4>
+            </div>
+          </div>
+          <div className="skillCard sass">
+            <div className="cardTitle">
+              <img src={Sass} alt="Sass icon" />
+              <h4>Sass</h4>
+            </div>
+          </div>
+          <div className="skillCard ux">
+            <div className="cardTitle">
+              <img src={UX} alt="UX icon" />
+              <h4>User Experience</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
