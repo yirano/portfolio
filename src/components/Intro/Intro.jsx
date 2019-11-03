622ffa4ab38b855a416b69135afb941e265f2daf
yirano <yirano@protonmail.ch>
2019-11-03 18:15:02 -0500

    Fetch github API

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,17 +1,21 @@
-import React from "react";
-import "./Intro.css";
-import "../../fonts/fonts.css";
+import React, { Component } from 'react';
+import './Intro.css';
+import '../../fonts/fonts.css';
 
-export default function Intro() {
-  return (
-    <section className="heading" id="intro">
-      <div className="headingTitle">
-        <p className="alignLeft uppercase caption">My name is Tiffany</p>
-        <h1>Hello!</h1>
-        <p className="alignRight uppercase caption">
-          &amp; I'm a <span className="highlight">Frontend Developer</span>
-        </p>
-      </div>
-    </section>
-  );
+export class Intro extends Component {
+	render() {
+		return (
+			<section className="heading" id="intro">
+				<div className="headingTitle">
+					<p className="alignLeft uppercase caption">My name is Tiffany</p>
+					<h1>Hello!</h1>
+					<p className="alignRight uppercase caption">
+						&amp; I'm a <span className="highlight">Frontend Developer</span>
+					</p>
+				</div>
+			</section>
+		);
+	}
 }
+
+export default Intro;
