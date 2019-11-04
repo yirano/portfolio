2104884310cb78d97ce1e0f1450a46d870af344f
yirano <yirano@protonmail.ch>
2019-11-04 17:17:55 -0500

    Begin onMouseEnter functions for Project section

--- src/components/Projects/project-components/ProjectBook.jsx
+++ src/components/Projects/project-components/ProjectBook.jsx
@@ -1,35 +1,41 @@
-import React, { Component } from 'react';
-import projects from './ProjectLibrary';
+import React, { Component } from "react";
+import projects from "./ProjectLibrary";
 
 export class ProjectBook extends Component {
-	constructor() {
-		super();
-		this.state = {
-			projects: projects
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      projects: projects
+    };
+  }
 
-	flip = () => {
-		console.log('entered');
-		this.setState((prevState) => {
-			this.state.projects.map((project) => {
-				project.active = !project.active;
-			});
-		});
-	};
+  flip = item => {
+    console.log(this.id);
+    this.state.projects.map(project => {
+      this.setState(prevState => {
+        return (project.active = !project.active);
+      });
+      return project.active;
+    });
+  };
 
-	render(props) {
-		return (
-			<div className="projectBook">
-				{this.state.projects.map((item) => (
-					<div key={item.id} className="projectShelfCard" onMouseEnter={this.flip}>
-						<img src={item.image} alt="" />
-						<h3 className="projectShelfTitle">{item.projectName}</h3>
-					</div>
-				))}
-			</div>
-		);
-	}
+  render(props) {
+    return (
+      <div className="projectBook">
+        {this.state.projects.map(({ id, image, projectName }) => (
+          <div
+            key={id}
+            className="projectShelfCard"
+            onMouseEnter={() => this.flip(id)}
+            onMouseLeave={this.flip}
+          >
+            <img src={image} alt="" />
+            <h3 className="projectShelfTitle">{projectName}</h3>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default ProjectBook;
--- src/components/Projects/project-components/ProjectLibrary.jsx
+++ src/components/Projects/project-components/ProjectLibrary.jsx
@@ -1,33 +1,33 @@
-import BookmarkLP from '../../../images/projectImgs/BookmarkLP.jpg';
+import BookmarkLP from "../../../images/projectImgs/BookmarkLP.jpg";
 
 const projectLibrary = [
-	{
-		id: 0,
-		image: BookmarkLP,
-		projectName: 'Bookmark Landing Page',
-		projectGit: 'https://github.com/yirano/react-bookmark_lp',
-		projectURL: 'https://yirano.github.io/react-bookmark_lp/',
-		projectLang: [ 'html', 'css', 'javascript', 'react' ],
-		active: true
-	},
-	{
-		id: 1,
-		image: BookmarkLP,
-		projectName: 'Four Card Feature',
-		projectGit: 'https://github.com/yirano/frontend-four_card_feature',
-		projectURL: 'https://yirano.github.io/frontend-four_card_feature/src/',
-		projectLang: [ 'html', 'css', 'javascript' ],
-		active: false
-	},
-	{
-		id: 2,
-		image: BookmarkLP,
-		projectName: 'Base Apparel',
-		projectGit: 'https://github.com/yirano/frontend-base_apparel',
-		projectURL: 'https://yirano.github.io/frontend-base_apparel/src/',
-		projectLang: [ 'html', 'css', 'javascript' ],
-		active: false
-	}
+  {
+    id: 0,
+    image: BookmarkLP,
+    projectName: "Bookmark Landing Page",
+    projectGit: "https://github.com/yirano/react-bookmark_lp",
+    projectURL: "https://yirano.github.io/react-bookmark_lp/",
+    projectLang: ["html", "css", "javascript", "react"],
+    active: false
+  },
+  {
+    id: 1,
+    image: BookmarkLP,
+    projectName: "Four Card Feature",
+    projectGit: "https://github.com/yirano/frontend-four_card_feature",
+    projectURL: "https://yirano.github.io/frontend-four_card_feature/src/",
+    projectLang: ["html", "css", "javascript"],
+    active: false
+  },
+  {
+    id: 2,
+    image: BookmarkLP,
+    projectName: "Base Apparel",
+    projectGit: "https://github.com/yirano/frontend-base_apparel",
+    projectURL: "https://yirano.github.io/frontend-base_apparel/src/",
+    projectLang: ["html", "css", "javascript"],
+    active: false
+  }
 ];
 
 export default projectLibrary;
