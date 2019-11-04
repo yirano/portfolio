f2b3de57f706684ffd48a0c5093b79c88d1866cc
yirano <yirano@protonmail.ch>
2019-11-04 14:12:40 -0500

    Add class to control flip display in project shelf

--- src/components/Projects/project-components/ProjectShelf.jsx
+++ src/components/Projects/project-components/ProjectShelf.jsx
@@ -1,23 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class ProjectShelf extends Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor() {
+    super();
+    this.state = {
+      flip: false
+    };
+  }
 
-	render(props) {
-		return (
-			<div className="projectShelf">
-				{this.props.items.map((item) => (
-					<div key={item.id} className="projectShelfCard">
-						<div className="imgControl" id="imgControl" />
-						<img src={item.image} alt="" />
-						<h3 className="projectShelfTitle">{item.projectName}</h3>
-					</div>
-				))}
-			</div>
-		);
-	}
+  flip = props => {
+    console.log(this.props.items);
+    this.setState(prevState => {
+      return (prevState.flip = !prevState.flip);
+    });
+  };
+
+  render(props) {
+    const flipCard = this.state.flip ? "cardFlip" : "";
+    return (
+      <div className="projectShelf">
+        {this.props.items.map(item => (
+          <div
+            key={item.id}
+            className="projectShelfCard"
+            onMouseEnter={this.flip}
+            onMouseLeave={this.flip}
+          >
+            <img src={item.image} alt="" className={flipCard} />
+            <h3 className="projectShelfTitle">{item.projectName}</h3>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default ProjectShelf;
