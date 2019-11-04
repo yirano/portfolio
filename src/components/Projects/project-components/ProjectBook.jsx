c37e5f28f539a515101bea2ec7b26a701546f2e3
yirano <yirano@protonmail.ch>
2019-11-04 17:26:57 -0500

    Flip Project card on hover

--- src/components/Projects/project-components/ProjectBook.jsx
+++ src/components/Projects/project-components/ProjectBook.jsx
@@ -1,41 +1,43 @@
-import React, { Component } from "react";
-import projects from "./ProjectLibrary";
+import React, { Component } from 'react';
+import projects from './ProjectLibrary';
 
 export class ProjectBook extends Component {
-  constructor() {
-    super();
-    this.state = {
-      projects: projects
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			projects: projects
+		};
+	}
 
-  flip = item => {
-    console.log(this.id);
-    this.state.projects.map(project => {
-      this.setState(prevState => {
-        return (project.active = !project.active);
-      });
-      return project.active;
-    });
-  };
+	flip = (id) => {
+		console.log(id);
+		this.state.projects.map((project) => {
+			this.setState((prevState) => {
+				if (project.id === id) {
+					return (project.active = !project.active);
+				}
+			});
+			return project.active;
+		});
+	};
 
-  render(props) {
-    return (
-      <div className="projectBook">
-        {this.state.projects.map(({ id, image, projectName }) => (
-          <div
-            key={id}
-            className="projectShelfCard"
-            onMouseEnter={() => this.flip(id)}
-            onMouseLeave={this.flip}
-          >
-            <img src={image} alt="" />
-            <h3 className="projectShelfTitle">{projectName}</h3>
-          </div>
-        ))}
-      </div>
-    );
-  }
+	render(props) {
+		return (
+			<div className="projectBook">
+				{this.state.projects.map(({ id, image, projectName, active }) => (
+					<div
+						key={id}
+						className="projectShelfCard"
+						onMouseEnter={() => this.flip(id)}
+						onMouseLeave={() => this.flip(id)}
+					>
+						<img src={image} alt="" className={active ? 'cardFlip' : ''} />
+						<h3 className="projectShelfTitle">{projectName}</h3>
+					</div>
+				))}
+			</div>
+		);
+	}
 }
 
 export default ProjectBook;
