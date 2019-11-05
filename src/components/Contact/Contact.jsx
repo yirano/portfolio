7bd3bde4db3af6078f136144cf452d170157e686
yirano <yirano@protonmail.ch>
2019-11-05 16:13:26 -0500

    Begin contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,10 +3,17 @@ import './Contact.css';
 
 export default function Contact() {
 	return (
-		<section className=" contactHeading" id="contact">
-			<h1>
-				Contact <span className="highlight">me</span> here
-			</h1>
+		<section className=" contact" id="contact">
+			<form action="">
+				<label htmlFor="name" />
+				<input type="text" name="name" id="name" placeholder="Name" required />
+				<label htmlFor="email" />
+				<input type="email" name="email" id="email" placeholder="Email Address" required />
+				<label htmlFor="phone" />
+				<input type="tel" name="phone" id="phone" placeholder="Phone Number - optional" />
+				<label htmlFor="message" />
+				<input type="text" name="message" id="message" placeholder="Message" />
+			</form>
 		</section>
 	);
 }
--- src/components/Projects/project-components/ProjectBook.jsx
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import projects from './ProjectLibrary';
-
-export class ProjectBook extends Component {}
-
-export default ProjectBook;
