024657e566e47e1bb02c60f9f44a9db62bd4f512
yirano <yirano@protonmail.ch>
2019-11-06 02:16:48 -0500

    Send contact form contents to email

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,19 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react';
+import * as emailjs from 'emailjs-com';
 import './Contact.css';
-
-export default function Contact() {
-	return (
-		<section className=" contact" id="contact">
-			<form action="">
-				<label htmlFor="name" />
-				<input type="text" name="name" id="name" placeholder="Name" required />
-				<label htmlFor="email" />
-				<input type="email" name="email" id="email" placeholder="Email Address" required />
-				<label htmlFor="phone" />
-				<input type="tel" name="phone" id="phone" placeholder="Phone Number - optional" />
-				<label htmlFor="message" />
-				<input type="text" name="message" id="message" placeholder="Message" />
-			</form>
-		</section>
-	);
+class Contact extends Component {
+	state = {
+		name: '',
+		email: '',
+		subject: '',
+		message: ''
+	};
+	handleSubmit(e) {
+		e.preventDefault();
+		const { name, email, subject, message } = this.state;
+		let templateParams = {
+			email: email,
+			from_name: name,
+			to_name: 'user_Ph7Q4dFDfBXaCF6O82qSl',
+			subject: subject,
+			message_html: message
+		};
+		emailjs.send('gmail', 'template_eOv2qlYU', templateParams, 'user_Ph7Q4dFDfBXaCF6O82qSl');
+		this.resetform();
+	}
+	resetform() {
+		this.setState({
+			name: '',
+			email: '',
+			subject: '',
+			message: ''
+		});
+	}
+	handleChange = (param, e) => {
+		this.setState({ [param]: e.target.value });
+	};
+	render() {
+		return (
+			<div>
+				<h3>Send me a Message!</h3>
+				<form onSubmit={this.handleSubmit.bind(this)}>
+					<label className="text-muted" />
+					<input
+						type="email"
+						name="email"
+						value={this.state.email}
+						className="text-primary"
+						onChange={this.handleChange.bind(this, 'email')}
+						placeholder="Enter email"
+					/>
+					<label className="text-muted" />
+					<input
+						type="text"
+						name="name"
+						value={this.state.name}
+						className="text-primary"
+						onChange={this.handleChange.bind(this, 'name')}
+						placeholder="Name"
+					/>
+					<label className="text-muted" />
+					<input
+						type="text"
+						name="subject"
+						className="text-primary"
+						value={this.state.subject}
+						onChange={this.handleChange.bind(this, 'subject')}
+						placeholder="Subject"
+					/>
+					<label className="text-muted" />
+					<textarea
+						type="text"
+						name="message"
+						className="text-primary"
+						value={this.state.message}
+						onChange={this.handleChange.bind(this, 'message')}
+					/>
+					<button variant="primary" type="submit">
+						Submit
+					</button>
+				</form>
+			</div>
+		);
+	}
 }
+export default Contact;
