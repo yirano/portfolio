af94477e49abff0a6f082f5ffd07ea262618031d
yirano <yirano@protonmail.ch>
2019-11-05 23:05:02 -0500

    Clean up redundant files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from "react";
-import "./fonts/fonts.css";
-import "./App.css";
-import Header from "./components/Header/Header";
-import Intro from "./components/Intro/Intro";
-import Skills from "./components/Skills/Skills";
-import Projects from "./components/Projects/Projects";
-// import About from "./components/About/About";
-import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
+import React from 'react';
+import './fonts/fonts.css';
+import './App.css';
+import Header from './components/Header/Header';
+import Intro from './components/Intro/Intro';
+import Skills from './components/Skills/Skills';
+import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 function App() {
-  return (
-    <main>
-      <Header />
-      <Intro />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
-    </main>
-  );
+	return (
+		<main>
+			<Header />
+			<Intro />
+			<Skills />
+			<Projects />
+			<Contact />
+			<Footer />
+		</main>
+	);
 }
 
 export default App;
