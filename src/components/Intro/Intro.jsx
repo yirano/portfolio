3815b78ca976e97a11765b9160a63630afea96af
yirano <yirano@protonmail.ch>
2019-11-06 20:29:04 -0500

    Add background to hero

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,26 +2,16 @@ import React, { Component } from 'react';
 import './Intro.css';
 import '../../fonts/fonts.css';
 // import vid from '../../images/videos/sky.mp4';
-import profile from '../../images/self.JPG';
+// import profile from '../../images/self.JPG';
 
 export class Intro extends Component {
 	render() {
 		return (
 			<section className="hero" id="hero">
-				<div className="leftHero">
-					<div className="imageFrame">
-						<img className="profileImg" src={profile} alt="self" />
-					</div>
-				</div>
-				<div className="rightHero">
-					<div className="heroTextContainer">
-						<h2>Welcome!</h2>
-						<p>
-							My name is Tiffany. I'm a Frontend Developer with a focus in developing with the user
-							experience in mind. I'm also a self proclaimed JavaScript junkie.
-						</p>
-					</div>
-				</div>
+        <div className="background" />
+        <div className="heroText">
+          <h1>Yirano</h1>
+        </div>
 			</section>
 		);
 	}
