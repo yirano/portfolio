94f886f74ca896a625fb545c3835fbe98e666dbc
yirano <yirano@protonmail.ch>
2019-11-08 08:08:38 -0500

    Add responsive menu icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,50 +5,63 @@ import Twitter from '../../images/SVG/twitter.svg';
 import './Header.css';
 
 export class Header extends Component {
-	// componentDidMount() {
-	// 	const nav = document.getElementById('nav');
-	// 	window.addEventListener('scroll', () => {
-	// 		return window.scrollY > 10 ? (nav.classList = 'sticky') : (nav.classList = '');
-	// 	});
-	// }
+	constructor() {
+		super();
+		this.state = {
+			menuActive: false
+		};
+	}
+
+	menuActive = () => {
+		this.setState((prevState) => {
+			return (prevState.menuActive = !prevState.menuActive);
+		});
+	};
 	render() {
 		return (
 			<header>
-				<nav id="nav">
-					<div className="navLinks">
-						<ul>
-							<li>
-								<a href="#intro">Home</a>
-							</li>
-							<li>
-								<a href="#projects">Projects</a>
-							</li>
-							<li>
-								<a href="#aboutMe">About Me</a>
-							</li>
-							<li>
-								<a href="#contact">Contact</a>
-							</li>
-							<li>
-								<a href="https://twitter.com/yiirano" target="_blank" rel="noopener noreferrer">
-									<img src={Twitter} alt="Twitter link" />
-								</a>
-							</li>
-							<li>
-								<a href="https://github.com/yirano" target="_blank" rel="noopener noreferrer">
-									<img src={Github} alt="Github link" />
-								</a>
-							</li>
-							<li>
-								<a href="https://www.linkedin.com/in/yt-arellano/"
-									target="_blank"
-									rel="noopener noreferrer">
-									<img src={LinkedIn} alt="LinkedIn link" />
-								</a>
-							</li>
-						</ul>
-					</div>
-				</nav>
+				<button className="menuIcon" onClick={this.menuActive}>
+					CLICK
+				</button>
+				{this.state.menuActive ? (
+					<nav id="nav">
+						<div className="navLinks">
+							<ul>
+								<li>
+									<a href="#intro">Home</a>
+								</li>
+								<li>
+									<a href="#projects">Projects</a>
+								</li>
+								<li>
+									<a href="#aboutMe">About Me</a>
+								</li>
+								<li>
+									<a href="#contact">Contact</a>
+								</li>
+								<li>
+									<a href="https://twitter.com/yiirano" target="_blank" rel="noopener noreferrer">
+										<img src={Twitter} alt="Twitter link" />
+									</a>
+								</li>
+								<li>
+									<a href="https://github.com/yirano" target="_blank" rel="noopener noreferrer">
+										<img src={Github} alt="Github link" />
+									</a>
+								</li>
+								<li>
+									<a
+										href="https://www.linkedin.com/in/yt-arellano/"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<img src={LinkedIn} alt="LinkedIn link" />
+									</a>
+								</li>
+							</ul>
+						</div>
+					</nav>
+				) : null}
 			</header>
 		);
 	}
