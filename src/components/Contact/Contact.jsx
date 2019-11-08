484dee31926f33dd81afaaae13701b6ab753fc85
yirano <yirano@protonmail.ch>
2019-11-07 20:01:15 -0500

    Remove section title in contacts out of the flow of two body elements of form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -35,58 +35,60 @@ class Contact extends Component {
 	render() {
 		return (
 			<section className="contactContainer" id="contact">
-				<form onSubmit={this.handleSubmit.bind(this)}>
-					<div className="leftContact">
-						<div className="sectionTitle">
-							<h3>I'd love to hear from you!</h3>
-						</div>
+				<div className="sectionTitle">
+					<h3>I'd love to hear from you!</h3>
+				</div>
 
-						<label htmlFor="name" />
-						<input
-							type="text"
-							name="name"
-							value={this.state.name}
-							className="text-primary"
-							onChange={this.handleChange.bind(this, 'name')}
-							placeholder="Name"
-							required
-						/>
-						<label htmlFor="email" />
-						<input
-							type="email"
-							name="email"
-							value={this.state.email}
-							className="text-primary"
-							onChange={this.handleChange.bind(this, 'email')}
-							placeholder="Your Email"
-							required
-						/>
-						<label htmlFor="subject" />
-						<input
-							type="text"
-							name="subject"
-							className="text-primary"
-							value={this.state.subject}
-							onChange={this.handleChange.bind(this, 'subject')}
-							placeholder="Subject"
-							required
-						/>
-					</div>
-					<div className="rightContact">
-						<label htmlFor="message" />
-						<textarea
-							type="text"
-							name="message"
-							className="text-primary"
-							value={this.state.message}
-							onChange={this.handleChange.bind(this, 'message')}
-							placeholder="Write your message here"
-						/>
-						<button variant="primary" type="submit">
-							Send Message
-						</button>
-					</div>
-				</form>
+				<div className="contactFlexwrap">
+					<form onSubmit={this.handleSubmit.bind(this)}>
+						<div className="leftContact">
+							<label htmlFor="name" />
+							<input
+								type="text"
+								name="name"
+								value={this.state.name}
+								className="text-primary"
+								onChange={this.handleChange.bind(this, 'name')}
+								placeholder="Name"
+								required
+							/>
+							<label htmlFor="email" />
+							<input
+								type="email"
+								name="email"
+								value={this.state.email}
+								className="text-primary"
+								onChange={this.handleChange.bind(this, 'email')}
+								placeholder="Your Email"
+								required
+							/>
+							<label htmlFor="subject" />
+							<input
+								type="text"
+								name="subject"
+								className="text-primary"
+								value={this.state.subject}
+								onChange={this.handleChange.bind(this, 'subject')}
+								placeholder="Subject"
+								required
+							/>
+						</div>
+						<div className="rightContact">
+							<label htmlFor="message" />
+							<textarea
+								type="text"
+								name="message"
+								className="text-primary"
+								value={this.state.message}
+								onChange={this.handleChange.bind(this, 'message')}
+								placeholder="Write your message here"
+							/>
+							<button variant="primary" type="submit">
+								Send Message
+							</button>
+						</div>
+					</form>
+				</div>
 			</section>
 		);
 	}
