48425fe0e97f3ba68dade13d6d81750e12486c09
yirano <yirano@protonmail.ch>
2019-11-11 13:26:14 -0500

    Updates

---
+++ precache-manifest.4758b50d1172f00063e659b053038206.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fb565a67680fb66f0067a1bf4152add",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f7147ac3158e438f6a9",
+    "url": "/static/css/main.0420353e.chunk.css"
+  },
+  {
+    "revision": "3507092a2997e3644a13",
+    "url": "/static/js/2.4694b0fe.chunk.js"
+  },
+  {
+    "revision": "1f7147ac3158e438f6a9",
+    "url": "/static/js/main.5f54aac2.chunk.js"
+  },
+  {
+    "revision": "7d34b7a885d63b82ef0d",
+    "url": "/static/js/runtime-main.a93af8c9.js"
+  },
+  {
+    "revision": "66046f5a327337f6085de842e6e43a9e",
+    "url": "/static/media/Apple.66046f5a.jpg"
+  },
+  {
+    "revision": "1cb8b8de93188080047d719a2cc95b7f",
+    "url": "/static/media/BaseApparel.1cb8b8de.jpg"
+  },
+  {
+    "revision": "2b67dc475ff6420b55bbcc7debc44775",
+    "url": "/static/media/BookmarkLP.2b67dc47.jpg"
+  },
+  {
+    "revision": "956947071c6e0265a0ffe62a984d7e90",
+    "url": "/static/media/FashionBlog.95694707.jpg"
+  },
+  {
+    "revision": "de7f643d8a4dfc698fd2ecd9dfb0f405",
+    "url": "/static/media/FourCard.de7f643d.jpg"
+  },
+  {
+    "revision": "317df18a8663639f2aed24e251f6f26e",
+    "url": "/static/media/ProjectIntro.317df18a.jpg"
+  },
+  {
+    "revision": "6d58394ecd15317e637b4a088206d52a",
+    "url": "/static/media/github.6d58394e.svg"
+  },
+  {
+    "revision": "dc12fb9067a6b3167075831dc8617739",
+    "url": "/static/media/headerImg.dc12fb90.jpeg"
+  },
+  {
+    "revision": "1fb77bbd01016ea126f652a273579814",
+    "url": "/static/media/linkedin.1fb77bbd.svg"
+  },
+  {
+    "revision": "105f0f107592965746a91241b566409f",
+    "url": "/static/media/responsive.105f0f10.svg"
+  },
+  {
+    "revision": "0ae8e725f50a82eb69f15b3d21ffadee",
+    "url": "/static/media/self.0ae8e725.JPG"
+  },
+  {
+    "revision": "e279f33f81cffb236db9e0216da2d97b",
+    "url": "/static/media/twitter.e279f33f.svg"
+  }
+]);
