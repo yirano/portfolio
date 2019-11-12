6690309127e81cc36b779e2c0b99579270cd53bf
yirano <yirano@protonmail.ch>
2019-11-12 08:53:26 -0500

    Convert heaader to 'newspaper' inspiration

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import './About.css';
-import Profile from '../../images/self.png';
+import Profile from '../../images/self.JPG';
 
 export class About extends Component {
 	render() {
 		return (
 			<section className="about" id="about">
+				<div className="topContent">
+					<h3>My name is Tiffany, and I'm a Frontend Developer.</h3>
+				</div>
 				<div className="aboutLeftContent">
 					<img src={Profile} alt="Profile" className="profileImage" />
 				</div>
 				<div className="aboutRightContent">
 					<div className="aboutTextWrap">
-						<h2>I'm a Frontend Developer</h2>
 						<p>
 							My strength lies in development but combined with my experience in design and user testing,
 							I create with the user's experience in mind.
