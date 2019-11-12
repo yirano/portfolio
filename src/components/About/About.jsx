2a166ee019b44e5625e4c855ca3d7fc2b49267fb
yirano <yirano@protonmail.ch>
2019-11-12 10:36:42 -0500

    Implement CSS grids into about me section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import './About.css';
-import Profile from '../../images/self.JPG';
+import React, { Component } from "react";
+import "./About.css";
+import Profile from "../../images/self.JPG";
 
 export class About extends Component {
-	render() {
-		return (
-			<section className="about" id="about">
-				<div className="topContent">
-					<h3>My name is Tiffany, and I'm a Frontend Developer.</h3>
-				</div>
-				<div className="aboutLeftContent">
-					<img src={Profile} alt="Profile" className="profileImage" />
-				</div>
-				<div className="aboutRightContent">
-					<div className="aboutTextWrap">
-						<p>
-							My strength lies in development but combined with my experience in design and user testing,
-							I create with the user's experience in mind.
-						</p>
-					</div>
-				</div>
-			</section>
-		);
-	}
+  render() {
+    return (
+      <section className="about" id="about">
+        <div className="topContent">
+          <h3>My name is Tiffany &amp; I'm a Frontend Developer.</h3>
+        </div>
+        <div className="aboutLeftContent">
+          <img src={Profile} alt="Profile" className="profileImage" />
+        </div>
+        <div className="aboutRightContent">
+          <div className="aboutTextWrap">
+            <p>
+              My strength lies in development but combined with my experience in
+              design and user testing, I create with the user's experience in
+              mind.
+            </p>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default About;
